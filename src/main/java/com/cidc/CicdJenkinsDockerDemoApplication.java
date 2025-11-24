610d73d911c77d7674a7e49e266a26c0672b87a9
github.com/MRSB-01 <sachinbadgujar7777@gmail.com>
2025-11-24 15:11:20 +0530

    first commit

---
+++ src/main/java/com/cidc/CicdJenkinsDockerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cidc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdJenkinsDockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdJenkinsDockerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cidc/CicdJenkinsDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cidc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdJenkinsDockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
